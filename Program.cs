823568836237b3a1e7c919f1eeaf8f0e15a5bcc4
EvgeniySh77 <evgen.sh77@mail.ru>
2022-01-24 21:03:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace task_5_1_6
+{
+    class Program
+    {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+        static void Main()
+        {
+            var arr = GetArrayFromConsole();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        (arr[i], arr[j]) = (arr[j], arr[i]);
+                    }
+                }
+                
+            }
+
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
